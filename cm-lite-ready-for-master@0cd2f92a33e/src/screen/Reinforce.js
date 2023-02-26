f4fb077131abd9e3bcd69561f0092012a4546f2e
hari <haricoolhead93@gmail.com>
2023-02-26 21:50:52 +0530

    removed

--- cm-lite-ready-for-master@0cd2f92a33e/src/screen/Reinforce.js
+++
@@ -1,5 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Reinforce() {
-  return <Text>Hi Reinforce.</Text>;
-}
--- cm-lite-ready-for-master@0cd2f92a33e/src/screen/etrReach.js
+++
@@ -1,5 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function etrReach() {
-  return <Text>Hi etr Reach Page</Text>;
-}
